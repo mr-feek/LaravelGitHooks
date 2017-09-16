87032402e85b84c175fd4c9c26ae0a734047c8d8
Feek <fpm5022@gmail.com>
2017-09-16 15:32:09 -0700

    whoopsy, i dont know bash

--- src/Hooks/pre-commit.sh
+++ src/Hooks/pre-commit.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 php artisan hooks:pre-commit
 
-if [ ! $? -eq 0 ]; then
-    echo "COMMIT REJECTED."
-    exit $?
+STATUS_CODE=$?
+if [ ! ${STATUS_CODE} -eq 0 ]; then
+    echo "COMMIT REJECTED: ${STATUS_CODE}"
+    exit ${STATUS_CODE}
 fi
 
 exit 0;
--- src/Hooks/pre-push.sh
+++ src/Hooks/pre-push.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 php artisan hooks:pre-push
 
-if [ ! $? -eq 0 ]; then
-    echo "PUSH REJECTED."
-    exit $?
+STATUS_CODE=$?
+if [ ! ${STATUS_CODE} -eq 0 ]; then
+    echo "PUSH REJECTED: ${STATUS_CODE}"
+    exit ${STATUS_CODE}
 fi
 
 exit 0;
