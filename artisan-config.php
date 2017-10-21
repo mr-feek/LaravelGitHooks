ead858a1e5a42c105cb0c010c0c1feb1c55e822e
Feek <fpm5022@gmail.com>
2017-10-21 16:07:56 -0700

    implement artisan stub for testing

---
+++ artisan-config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'commands' => [
+        \Feek\LaravelGitHooks\Commands\InstallHooks::class,
+        \Feek\LaravelGitHooks\Commands\CommitHooks\PreCommit::class,
+        \Feek\LaravelGitHooks\Commands\CommitHooks\PrepareCommitMsg::class,
+        \Feek\LaravelGitHooks\Commands\CommitHooks\PrePush::class,
+        \Feek\LaravelGitHooks\Commands\Sniff\Phpcs::class,
+    ],
+];
