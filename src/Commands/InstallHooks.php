52973391f8ad957e68c8e543f05816b5b5496375
Stefan Rempe <stefan.rempe95@gmail.com>
2017-10-22 17:31:59 +0200

    do not overwrite existing git hooks

--- src/Commands/InstallHooks.php
+++ src/Commands/InstallHooks.php
@@ -7,22 +7,6 @@ use Symfony\Component\Finder\Finder;
 class InstallHooks extends BaseCommand
 {
     /**
-     * @var Finder
-     */
-    private $finder;
-
-
-    /**
-     * InstallHooks constructor.
-     * @param Finder $finder
-     */
-    public function __construct(Finder $finder)
-    {
-        parent::__construct();
-        $this->finder = $finder;
-    }
-
-    /**
      * The name and signature of the console command.
      *
      * @var string
@@ -37,25 +21,52 @@ class InstallHooks extends BaseCommand
     protected $description = 'Installs all laravel-git-hooks to the local .git hooks folder';
 
     /**
+     * @var Finder
+     */
+    private $finder;
+
+    /**
+     * InstallHooks constructor.
+     *
+     * @param Finder $finder
+     */
+    public function __construct(Finder $finder)
+    {
+        parent::__construct();
+        $this->finder = $finder;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        if (!config('hooks.enabled')) {
+        if (! config('hooks.enabled')) {
             return 0;
         }
-        
-        $srcPath = __DIR__ . '/../Hooks/';
-        $destPath = base_path() . '/.git/hooks/';
+
+        $srcPath = __DIR__.'/../Hooks/';
+        $destPath = base_path().'/.git/hooks/';
 
         $this->finder->files()->in($srcPath)->name('*.sh');
 
         foreach ($this->finder as $file) {
             $source = $file->getRealPath();
-            $destination = $destPath . $file->getRelativePathname();
-            copy($source, $destination);
+            $destination = $destPath.$file->getRelativePathname();
+
+            $sourceContent = PHP_EOL.'# LARAVEL GIT HOOKS BEGIN #'.PHP_EOL.$file->getContents().PHP_EOL.'# LARAVEL GIT HOOKS END #'.PHP_EOL;
+            $destinationContent = file_get_contents($destination);
+
+            if (str_contains($destinationContent, '# LARAVEL GIT HOOKS BEGIN #')) {
+                $search = '/# LARAVEL GIT HOOKS BEGIN #[\s\S]+?LARAVEL GIT HOOKS END #/m';
+                $destinationContent = preg_replace($search, $sourceContent, $destinationContent);
+            } else {
+                $destinationContent .= $sourceContent;
+            }
+
+            file_put_contents($destination, $destinationContent);
             chmod($destination, 0775);
 
             $from = str_replace(base_path(), '', realpath($source));
